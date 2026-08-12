41b1080fa73b7416ccd5789099c8c8dd7ec409a0
steklobiz <vitraji@narod.ru>
2026-08-12 10:07:13 +0300

    fix config

--- app/config/config.hpp
+++ app/config/config.hpp
@@ -6,7 +6,7 @@
 
 #include <cstdint>
 
-// Application level config constants
+
 namespace app::config
 {
     namespace profiles
@@ -16,9 +16,21 @@ namespace app::config
     }
     namespace pid
     {
-        inline constexpr uint8_t output_max_power = 100;
+        // do not replace int32_t with uint8_t. All pid arithmetic based on int32_t
+        inline constexpr int32_t scale = 1000;
+        
+        // Uutput limits (heater duty range 0..100).
+        inline constexpr int32_t output_min_power = 100;
+        inline constexpr int32_t output_max_power = 100;
     }
-
+    namespace history
+    {
+        inline constexpr uint8_t event_capacity = 10;
+        inline constexpr uint8_t sample_capacity = 60;
+        inline constexpr uint32_t sample_period_s = 1; 
+    }
+    
+    
 inline constexpr uint8_t tc_count      = 2;
 
 inline constexpr uint8_t output_count      = 4;
